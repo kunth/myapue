762ca82e49ec026f6e0c6ba77420fd7a7bef506f
Jun Liu <fibonacci@zju.edu.cn>
2017-12-05 00:20:03 +0800

    record a mistake of using lseek

--- lseek.c
+++ lseek.c
@@ -9,3 +9,8 @@ main() {
     printf("seek OK\n");
   return 0;
 }
+
+// 记录用lseek踩的一个坑
+// open了一个文件后，用off_t len = lseek(fd, 0, SEEK_END)拿到了该文件的总字符数，然后再去read文件，read出来的值总是0
+// 是因为lseek(fd, 0, SEEK_END)已经把fd的文件位置设置成文件末尾了，需要用lseek(fd, 0, SEEK_SET)将文件指针设置回头部，
+// 再read出来就ok了
